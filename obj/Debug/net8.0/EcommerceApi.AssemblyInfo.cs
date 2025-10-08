e045681198f4f5e240c16bcc9036200c51cb91d2
Mustafa Gurhan <mgurhan13@hotmail.com>
2025-10-08 15:30:12 +0300

    FrontEnd

--- obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db96fa9e64a8d2ffb27b49a5e9648ede69b6501f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
